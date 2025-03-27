c603085d26e89e338d2fcfe66665a63466c43008
Kswooksw0 <kimsangwoothomas@gmail.com>
2025-03-27 19:32:51 +0800

    added required dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.module.css";
+import marmotClose from "./assets/images/screaming_marmot_close.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <img src={marmotClose}></img>
+  </div>;
 }
 
 export default App;
